1794594abb4ecee44fea6781b594cc44bac3e106
Franco Armando <francoarmando1911@gmail.com>
2025-01-19 22:00:06 -0300

    botones de editar y eliminar

--- src/components/PatientDetails.tsx
+++ src/components/PatientDetails.tsx
@@ -11,8 +11,25 @@ export default function PatientDetails({ patient }: PatientDetailsProps) {
             <PatientDetailItem label="Nombre" data={patient.name} />
             <PatientDetailItem label="Propietario" data={patient.caretaker} />
             <PatientDetailItem label="Email" data={patient.email} />
-            <PatientDetailItem label="Fecha" data={patient.date instanceof Date ? patient.date.toLocaleDateString() : patient.date || "No disponible"} />
+            <PatientDetailItem label="Fecha" data={patient.date.toString()} />
             <PatientDetailItem label="Sintomas" data={patient.symptoms} />
+
+            <div className="flex justify-between mt-10">
+                <button 
+                    type="button"
+                    className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-xl"
+                >
+                    Editar
+                </button>
+
+                <button
+                    type="button"
+                    className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-xl"
+                >
+                    Eliminar
+                </button>
+
+            </div>
         </div>
     )
 }
